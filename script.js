dba306e16d1a6020c27a97404e73746ec8843a4d
margaretyjkim <margaret.yj.kim@gmail.com>
2016-03-27 14:45:10 -0400

    add basic line through once checked off

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+var i = 0;
+
 function todoForm() {
+	i++;
+
 	var task = document.taskForm.task.value;
-	var taskInfo = '<div id="todoInfo"><input type="checkbox" />' + task + '</div>';
+	var taskInfo = '<div id="todoInfo' + i + '"><input type="checkbox" onclick="crossedOut(\'todoInfo' + i + '\')" />' + task + '</div>';
 	document.getElementById("todoInfo").innerHTML += taskInfo;
+}
+
+function crossedOut(item) {
+	document.getElementById(item).className = "checkedOff";
 }
